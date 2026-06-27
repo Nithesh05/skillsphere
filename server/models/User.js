a83543fc9c47cb35da802f86f83ed180dc8687ed
Nithesh <nitheshcsk05@gmail.com>
2026-06-27 11:55:09 +0530

    Add isVerified field to User schema

--- server/models/User.js
+++ server/models/User.js
@@ -89,6 +89,10 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: true,
     },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
